513a6f1eec643b0cf2c1a25ca255c7170f97fd68
George Mokbel <george@withhugo.com>
2025-05-29 06:22:14 -0400

    Fix bug where number of pull requests evaluated isn't correctly done.

--- src/index.ts
+++ src/index.ts
@@ -1,121 +1,164 @@
 import { Octokit } from "octokit";
 
-if (!process.env.GITHUB_TOKEN || !process.env.GITHUB_OWNER || !process.env.GITHUB_REPO) {
-    throw new Error('GITHUB_TOKEN, GITHUB_OWNER, and GITHUB_REPO must be set');
+if (
+  !process.env.GITHUB_TOKEN ||
+  !process.env.GITHUB_OWNER ||
+  !process.env.GITHUB_REPO
+) {
+  throw new Error("GITHUB_TOKEN, GITHUB_OWNER, and GITHUB_REPO must be set");
 }
 
 const githubAccessToken = process.env.GITHUB_TOKEN;
 const githubOwner = process.env.GITHUB_OWNER;
 const githubRepo = process.env.GITHUB_REPO;
 const targetBranch = process.env.TARGET_BRANCH;
-const numberClosedPullRequestsToEvaluate = process.env.NUMBER_CLOSED_PULL_REQUESTS_TO_EVALUATE ? Number(process.env.NUMBER_CLOSED_PULL_REQUESTS_TO_EVALUATE) : 50;
+const numberClosedPullRequestsToEvaluate = process.env
+  .NUMBER_CLOSED_PULL_REQUESTS_TO_EVALUATE
+  ? Number(process.env.NUMBER_CLOSED_PULL_REQUESTS_TO_EVALUATE)
+  : 50;
 
 const toMsDiff = (t1?: string | null, t2?: string | null): number => {
-    if (!t1 || !t2) {
-        return 0;
-    }
-    return Math.abs(new Date(t2).getTime() - new Date(t1).getTime());
+  if (!t1 || !t2) {
+    return 0;
+  }
+  return Math.abs(new Date(t2).getTime() - new Date(t1).getTime());
 };
 
 function msToReadableTimeDelta(ms: number): string {
-    let seconds = (ms / 1000).toFixed(1);
-    let minutes = (ms / (1000 * 60)).toFixed(1);
-    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
-    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-    if (Number(seconds) < 60)
-        return seconds + " Sec";
-    if (Number(minutes) < 60)
-        return minutes + " Min";
-    if (Number(hours) < 24)
-        return hours + " Hrs";
-    return days + " Days";
+  let seconds = (ms / 1000).toFixed(1);
+  let minutes = (ms / (1000 * 60)).toFixed(1);
+  let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+  let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+  if (Number(seconds) < 60) return seconds + " Sec";
+  if (Number(minutes) < 60) return minutes + " Min";
+  if (Number(hours) < 24) return hours + " Hrs";
+  return days + " Days";
 }
 
 function toReadableTimeDelta(t1?: string | null, t2?: string | null): string {
-    if (!t1 || !t2) {
-        return 'N/A';
-    }
-    return msToReadableTimeDelta(toMsDiff(t1, t2));
+  if (!t1 || !t2) {
+    return "N/A";
+  }
+  return msToReadableTimeDelta(toMsDiff(t1, t2));
 }
 
-
 (async () => {
+  const octokit = new Octokit({
+    auth: githubAccessToken,
+  });
 
-    const octokit = new Octokit({
-        auth: githubAccessToken,
-    });
+  let totalPullRequestsEvaluated = 0;
+  let averageTimeToMerge = 0;
+  let averageTimeToFirstReview = 0;
+  let averageTimeToSecondReview = 0;
+  let averageLinesAdded = 0;
+  let averageLinesRemoved = 0;
 
+  let page = 1;
+  while (totalPullRequestsEvaluated < numberClosedPullRequestsToEvaluate) {
     const closedPullRequests = await octokit.rest.pulls.list({
-        owner: githubOwner,
-        repo: githubRepo,
-        state: 'closed',
-        sort: 'created',
-        direction: 'desc',
-        per_page: numberClosedPullRequestsToEvaluate,
+      owner: githubOwner,
+      repo: githubRepo,
+      state: "closed",
+      sort: "created",
+      direction: "desc",
+      per_page: 100,
+      page,
     });
 
-    let totalPullRequestsEvaluated = 0;
-    let averageTimeToMerge = 0;
-    let averageTimeToFirstReview = 0;
-    let averageTimeToSecondReview = 0;
-    let averageLinesAdded = 0;
-    let averageLinesRemoved = 0;
+    if (closedPullRequests.data.length === 0) {
+      break;
+    }
 
     for (const pullRequest of closedPullRequests.data) {
-        const details = await octokit.rest.pulls.get({
-            owner: githubOwner,
-            repo: githubRepo,
-            pull_number: pullRequest.number,
-        });
+      const details = await octokit.rest.pulls.get({
+        owner: githubOwner,
+        repo: githubRepo,
+        pull_number: pullRequest.number,
+      });
 
-        // if not merged or not targeting dev branch, skip
-        if (!details.data.merged || details.data.base.ref !== targetBranch) {
-            continue;
-        }
+      // if not merged or not targeting dev branch, skip
+      if (!details.data.merged || details.data.base.ref !== targetBranch) {
+        continue;
+      }
 
-        totalPullRequestsEvaluated++;
+      const author = details.data.user?.login;
 
-        const reviews = await octokit.rest.pulls.listReviews({
-            owner: githubOwner,
-            repo: githubRepo,
-            pull_number: pullRequest.number,
-        });
+      totalPullRequestsEvaluated++;
 
-        const firstReview = reviews.data[0]?.submitted_at;
-        const firstReviewer = reviews.data[0]?.user?.login;
-        const ttfr = toReadableTimeDelta(details.data.created_at, firstReview);
-        averageTimeToFirstReview += toMsDiff(details.data.created_at, firstReview);
+      const reviews = await octokit.rest.pulls.listReviews({
+        owner: githubOwner,
+        repo: githubRepo,
+        pull_number: pullRequest.number,
+      });
 
 
-        const secondReview = reviews.data[1]?.submitted_at;
-        const secondReviewer = reviews.data[1]?.user?.login;
-        const ttsr = toReadableTimeDelta(details.data.created_at, secondReview);
-        averageTimeToSecondReview += toMsDiff(details.data.created_at, secondReview);
+      const firstReview = reviews.data[0]?.submitted_at;
+      const firstReviewer = reviews.data[0]?.user?.login;
+      const ttfr = toReadableTimeDelta(details.data.created_at, firstReview);
+      averageTimeToFirstReview += toMsDiff(
+        details.data.created_at,
+        firstReview
+      );
 
-        const by = details.data.user?.login;
-        const title = details.data.title;
-        const mergedAt = details.data.merged_at;
-        const ttm = toReadableTimeDelta(mergedAt, details.data.created_at);
-        averageTimeToMerge += toMsDiff(mergedAt, details.data.created_at);
+      const secondReview = reviews.data[1]?.submitted_at;
+      const secondReviewer = reviews.data[1]?.user?.login;
+      const ttsr = toReadableTimeDelta(details.data.created_at, secondReview);
+      averageTimeToSecondReview += toMsDiff(
+        details.data.created_at,
+        secondReview
+      );
 
-        const linesAdded = details.data.additions;
-        const linesRemoved = details.data.deletions;
-        averageLinesAdded += linesAdded;
-        averageLinesRemoved += linesRemoved;
+      const title = details.data.title;
+      const mergedAt = details.data.merged_at;
+      const ttm = toReadableTimeDelta(mergedAt, details.data.created_at);
+      averageTimeToMerge += toMsDiff(mergedAt, details.data.created_at);
 
-        console.log(`${title}
-Author: ${by}
+      const linesAdded = details.data.additions;
+      const linesRemoved = details.data.deletions;
+      averageLinesAdded += linesAdded;
+      averageLinesRemoved += linesRemoved;
+
+      console.log(`${title}
+Author: ${author}
 Time to merge: ${ttm}
 First review time: ${ttfr} (${firstReviewer})
 Second review time: ${ttsr} (${secondReviewer})
 Code changes: +${linesAdded}, -${linesRemoved}
 `);
+
+      if (totalPullRequestsEvaluated === numberClosedPullRequestsToEvaluate) {
+        break;
+      }
     }
 
-    console.log(`Total pull requests evaluated: ${totalPullRequestsEvaluated}`);
-    console.log(`Average time to merge: ${msToReadableTimeDelta(averageTimeToMerge / totalPullRequestsEvaluated)}`);
-    console.log(`Average time to first review: ${msToReadableTimeDelta(averageTimeToFirstReview / totalPullRequestsEvaluated)}`);
-    console.log(`Average time to second review: ${msToReadableTimeDelta(averageTimeToSecondReview / totalPullRequestsEvaluated)}`);
-    console.log(`Average lines added: ${Math.round(averageLinesAdded / totalPullRequestsEvaluated)}`);
-    console.log(`Average lines removed: ${Math.round(averageLinesRemoved / totalPullRequestsEvaluated)}`);
-})(); 
+    page += 1;
+  }
+
+  console.log(`Total pull requests evaluated: ${totalPullRequestsEvaluated}`);
+  console.log(
+    `Average time to merge: ${msToReadableTimeDelta(
+      averageTimeToMerge / totalPullRequestsEvaluated
+    )}`
+  );
+  console.log(
+    `Average time to first review: ${msToReadableTimeDelta(
+      averageTimeToFirstReview / totalPullRequestsEvaluated
+    )}`
+  );
+  console.log(
+    `Average time to second review: ${msToReadableTimeDelta(
+      averageTimeToSecondReview / totalPullRequestsEvaluated
+    )}`
+  );
+  console.log(
+    `Average lines added: ${Math.round(
+      averageLinesAdded / totalPullRequestsEvaluated
+    )}`
+  );
+  console.log(
+    `Average lines removed: ${Math.round(
+      averageLinesRemoved / totalPullRequestsEvaluated
+    )}`
+  );
+})();
